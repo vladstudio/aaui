31710a99c5918af3d3e4b0dbca691fb988807034
Vlad Gerasimov <vlad@vlad.studio>
2024-02-17 16:34:22 +0400

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  }),
+  integrations: [tailwind(), alpinejs()]
+});
