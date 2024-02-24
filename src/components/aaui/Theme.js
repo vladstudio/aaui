1bcd55ee1108281667929ad4c78be50109a3265b
Vlad Gerasimov <vlad@vlad.studio>
2024-02-24 08:12:49 +0400

    .

--- src/components/aaui/Theme.js
+++ src/components/aaui/Theme.js
@@ -2,9 +2,9 @@ export const Theme = {
   button: {
     common: "border rounded transition-all duration-75",
     default:
-      "border-stone-500/30 bg-white hover:bg-orange-50 hover:border-orange-500/30 active:shadow-[inset_0_2px_0_0_#11111108,0_1px_0px_1px_#FFF8,0_-1px_0px_1px_#00000005]",
+      "border-stone-500/30 bg-white hover:bg-orange-200/40 hover:border-orange-500/30 active:shadow-[inset_0_2px_0_0_#11111108,0_1px_0px_1px_#FFF8,0_-1px_0px_1px_#00000005]",
     primary: "bg-stone-800 hover:bg-orange-800 text-white",
-    ghost: "border-transparent hover:bg-orange-500/20",
+    ghost: "border-transparent hover:bg-orange-200/40",
   },
   backdrop: "bg-stone-300/30",
   dropdown:
