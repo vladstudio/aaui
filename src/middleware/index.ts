14bf63024c3b5208558294fc6b54659528b31a35
Vlad Gerasimov <vlad@vlad.studio>
2024-03-30 22:51:14 +0400

    dbPath

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,8 +3,10 @@
 import { defineMiddleware } from 'astro/middleware';
 import { Database } from "bun:sqlite"
 
+const dbPath = import.meta.env.MODE == 'development' ? "db/test.sqlite" : "/srv/aaui/db/test.sqlite"
+
 export const onRequest = defineMiddleware(async ({ locals }: any, next: () => any) => {
-  locals.db = new Database("db/test.sqlite")
+  locals.db = new Database(dbPath)
   locals.db.exec("PRAGMA journal_mode = WAL;")
   const response = await next();
   return response;
