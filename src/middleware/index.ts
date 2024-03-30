ec0209d83c864113301b7c40fdc23f922520e05a
Vlad Gerasimov <vlad@vlad.studio>
2024-03-30 23:02:39 +0400

    host

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -3,7 +3,7 @@
 import { defineMiddleware } from 'astro/middleware';
 import { Database } from "bun:sqlite"
 
-const dbPath = import.meta.env.MODE == 'development' ? "db/test.sqlite" : "/srv/aaui/db/test.sqlite"
+const dbPath = import.meta.env.HOST == 'local' ? "db/test.sqlite" : "/srv/aaui/db/test.sqlite"
 console.log(dbPath)
 
 export const onRequest = defineMiddleware(async ({ locals }: any, next: () => any) => {
